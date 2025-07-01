aa2bc83e73ae854d630e9626ed442c706a25af69
Conrad Waako K <68024054+elconrad23@users.noreply.github.com>
2025-07-02 01:56:20 +0300

    Update Emissions.cshtml.cs

--- TetragricUI/Pages/Emissions.cshtml.cs
+++ TetragricUI/Pages/Emissions.cshtml.cs
@@ -12,9 +12,9 @@ namespace TetragricUI.Pages
             // Dummy data until connected to API
             Emissions = new List<EmissionLog>
             {
-                new EmissionLog { Timestamp = DateTime.UtcNow.AddDays(-3), CO2Amount = 45.2 },
-                new EmissionLog { Timestamp = DateTime.UtcNow.AddDays(-2), CO2Amount = 32.8 },
-                new EmissionLog { Timestamp = DateTime.UtcNow.AddDays(-1), CO2Amount = 29.4 }
+                new EmissionLog { Timestamp = DateTime.UtcNow.AddDays(-3), CO2Amount = 45 },
+                new EmissionLog { Timestamp = DateTime.UtcNow.AddDays(-2), CO2Amount = 32 },
+                new EmissionLog { Timestamp = DateTime.UtcNow.AddDays(-1), CO2Amount = 29 }
             };
         }
     }
